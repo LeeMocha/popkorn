5816eb24d64207669a209b13894df6eee91798c5
User -2023NGLAA\Administrator <qwertui1217@gmail.com>
2024-03-28 12:19:44 +0900

    2024-03-28 payment entity => orderinfo entity 바꾸고 트랜잭션 적용

--- popkorn-back/src/main/java/com/teamstatic/popkornback/controller/PaymentsController.java
+++ popkorn-back/src/main/java/com/teamstatic/popkornback/controller/PaymentsController.java
@@ -1,129 +1,104 @@
-package com.teamstatic.popkornback.controller;
+   package com.teamstatic.popkornback.controller;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+   import java.io.IOException;
 
-import javax.servlet.http.HttpServletRequest;
+   import java.util.ArrayList;
+   import java.util.List;
+   import java.util.Map;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+   import org.springframework.web.bind.annotation.PostMapping;
+   import org.springframework.web.bind.annotation.RequestBody;
+   import org.springframework.web.bind.annotation.RequestMapping;
+   import org.springframework.web.bind.annotation.RestController;
 
-import com.siot.IamportRestClient.IamportClient;
-import com.siot.IamportRestClient.exception.IamportResponseException;
-import com.siot.IamportRestClient.request.CancelData;
-import com.siot.IamportRestClient.response.IamportResponse;
-import com.siot.IamportRestClient.response.Payment;
-import com.teamstatic.popkornback.entity.OrderDetail;
-import com.teamstatic.popkornback.entity.Product;
-import com.teamstatic.popkornback.entity.User;
-import com.teamstatic.popkornback.service.CartService;
-import com.teamstatic.popkornback.service.OrderDetailService;
-import com.teamstatic.popkornback.service.PaymentService;
-import com.teamstatic.popkornback.service.ProductService;
-import com.teamstatic.popkornback.service.UserService;
+   import com.siot.IamportRestClient.IamportClient;
+   import com.siot.IamportRestClient.exception.IamportResponseException;
+   import com.siot.IamportRestClient.request.CancelData;
+   import com.siot.IamportRestClient.response.IamportResponse;
+   import com.siot.IamportRestClient.response.Payment;
+   import com.teamstatic.popkornback.entity.OrderDetail;
+   import com.teamstatic.popkornback.entity.Product;
+   import com.teamstatic.popkornback.entity.User;
+   import com.teamstatic.popkornback.service.CartService;
+   import com.teamstatic.popkornback.service.OrderDetailService;
+   import com.teamstatic.popkornback.service.PaymentService;
+   import com.teamstatic.popkornback.service.ProductService;
+   import com.teamstatic.popkornback.service.UserService;
 
-import org.springframework.beans.factory.annotation.Autowired;
+   import lombok.extern.slf4j.Slf4j;
 
-@RequestMapping("/api/pay")
-@RestController
-public class PaymentsController {
+   import org.springframework.beans.factory.annotation.Autowired;
 
-   IamportClient iamportClient;
+   @Slf4j
+   @RequestMapping("/api/pay")
+   @RestController
+   public class PaymentsController {
 
-   @Autowired
-   PaymentService payService;
-   @Autowired
-   OrderDetailService odService;
-   @Autowired
-   ProductService pService;
-   @Autowired
-   CartService cService;
-   @Autowired
-   UserService uService;
+      IamportClient iamportClient;
 
-   private PaymentsController() {
-      this.iamportClient = new IamportClient("3803508415015286",
-            "hflvwap97oFoYSjRe3uTfEgy0CLNw0bXMfV7Og1UAUjgpS29O3buAQ3Lgw7ntgNNOjNfmIcDteoBNAzl");
-   }
-   
-   @PostMapping("/datatoserver")
-   public IamportResponse<Payment> kakaoPay(@RequestBody Map<String, Object> request)
-         throws IamportResponseException, IOException {
-      System.out.println(request.get("items"));
-      String imp_uid = (String) request.get("imp_uid");
-      String id = (String) request.get("id");
-      List<Map<String, Object>> items = (List<Map<String, Object>>) request.get("items");
-      List<OrderDetail> orderDetail = new ArrayList<>();
+      @Autowired
+      PaymentService payService;
+      @Autowired
+      OrderDetailService odService;
+      @Autowired
+      ProductService pService;
+      @Autowired
+      CartService cService;
+      @Autowired
+      UserService uService;
 
-      for (Map<String, Object> item : items) {
-         OrderDetail detail = new OrderDetail();
-         detail.setMerchantUid((String) item.get("merchantUid"));
-         detail.setPcode((int) item.get("pcode"));
-         detail.setProductname((String) item.get("productname"));
-         detail.setPrice((int) item.get("price"));
-         detail.setDetailcount((int) item.get("detailcount"));
-         detail.setAlternative((String) item.get("alternative"));
-         detail.setImage1((String) item.get("image1"));
-         orderDetail.add(detail);
-     }
+      private PaymentsController() {
+         this.iamportClient = new IamportClient("3803508415015286",
+               "hflvwap97oFoYSjRe3uTfEgy0CLNw0bXMfV7Og1UAUjgpS29O3buAQ3Lgw7ntgNNOjNfmIcDteoBNAzl");
+      }
+
+
+      @PostMapping("/datatoserver")
+      public IamportResponse<Payment> datatoserver(@RequestBody Map<String, Object> request)
+            throws IamportResponseException, IOException {
+
+               String imp_uid = (String) request.get("imp_uid");
+               String id = (String) request.get("id");
+               List<Map<String, Object>> items = (List<Map<String, Object>>) request.get("items");
+               List<OrderDetail> orderDetail = new ArrayList<>();
       
-      IamportResponse<Payment> paymentIamportResponse = iamportClient.paymentByImpUid(imp_uid);
-
-      Payment payment = paymentIamportResponse.getResponse();
-
-      for (OrderDetail product : orderDetail) {
-         if (product.getDetailcount() > pService.findByPcode(product.getPcode()).getStock()) {
+                  for (Map<String, Object> item : items) {
+                      OrderDetail detail = new OrderDetail();
+                      detail.setMerchantUid((String) item.get("merchantUid"));
+                      detail.setPcode((int) item.get("pcode"));
+                      detail.setProductname((String) item.get("productname"));
+                      detail.setPrice((int) item.get("price"));
+                      detail.setDetailcount((int) item.get("detailcount"));
+                      detail.setAlternative((String) item.get("alternative"));
+                      detail.setImage1((String) item.get("image1"));
+                      orderDetail.add(detail);
+                   }
+      
+               IamportResponse<Payment> paymentIamportResponse = iamportClient.paymentByImpUid(imp_uid);
+      
+               Payment payment = paymentIamportResponse.getResponse();
+               
+               // 재고가 주문량보다 적을 경우 주문 취소
+               for (OrderDetail product : orderDetail) {
+                 if (product.getDetailcount() > pService.findByPcode(product.getPcode()).getStock()) {
+                    iamportClient.cancelPaymentByImpUid(new CancelData(imp_uid, true));
+                    return null;
+                 }
+              }
+         
+         try {
+            
+            payService.savePaymentData(orderDetail, payment, id);
+            // 결과 주문내역 return (merchant_uid, email, ETC)
+            return paymentIamportResponse;
+            
+         } catch (Exception e) {
+            // 상기 try 부분에서 무결성에 문제가 생길경우 결제 취소.
+            log.info("결제 오류 발생!!! 결제를 취소합니다. err =>" + e);
             iamportClient.cancelPaymentByImpUid(new CancelData(imp_uid, true));
             return null;
          }
+
       }
-
-      // 결제 요청에 문제가 없어 Payment 테이블에 주문 내역 정보 저장.
-      com.teamstatic.popkornback.entity.Payment paymentEntity = com.teamstatic.popkornback.entity.Payment.builder()
-            .merchantUid(payment.getMerchantUid())
-            .buyerName(payment.getBuyerName())
-            .buyerEmail(payment.getBuyerEmail())
-            .buyerAddr(payment.getBuyerAddr())
-            .buyerPostcode(payment.getBuyerPostcode())
-            .buyerTel(payment.getBuyerTel())
-            .paidAmount(payment.getAmount())
-            .paidAt(payment.getPaidAt())
-            .status(payment.getStatus())
-            .build();
-      payService.savePaymentData(paymentEntity);
-
-      // 1.Product table => pcode에 해당하는 상품 stock 필드값 반영.
-      for (OrderDetail orderDetail2 : orderDetail) {
-         Product product = pService.findByPcode(orderDetail2.getPcode());
-         product.setStock(product.getStock() - orderDetail2.getDetailcount());
-         pService.save(product);
-      }
-
-      // 2.Cart table => 해당하는 id와 pcode의 cart 물품 삭제.
-      // 3. User table => 해당하는 id 의 reword 값 반영
-      // => 비회원인 경우 실행 안함.
-      System.out.println("***************"+id+"***********");
-      if (id != null) {
-         for (OrderDetail orderDetail2 : orderDetail) {
-            cService.deleteByIdAndPcode(id, orderDetail2.getPcode());
-         }
-         User user = uService.findById(id).get();
-         user.setReword(user.getReword() + (payment.getAmount().intValue() / 1000));
-         System.out.println((payment.getAmount().intValue() / 1000)); 
-         uService.save(user);
-      }
-
-      // 4. 해당 주문번호와 그 값들 OrderDetail 처리
-      // 위에 uService save 메서드 reword 반영안되는거 확인하기
-
-      return paymentIamportResponse;
 
    }
-
-}
