4ca06d0c1735ef8e105403424eda5281832674bb
User -2023NGLAA\Administrator <qwertui1217@gmail.com>
2024-03-29 14:07:09 +0900

    2024-03-29

--- popkorn/src/producPage/product/SubProduct.js
+++ popkorn/src/producPage/product/SubProduct.js
@@ -2,14 +2,16 @@
 import './SubProduct.css';
 import Slot2 from './slot2/Slot2';
 
-export default function SubProduct ({ servData }){
+export default function SubProduct({ servData }) {
     return (
-        <div className="subproduct_wrap">
-            {
-                servData.map((item, index) => 
-                        <Slot2 key={index} item={item} index={index}/>
-                )
-            }
-        </div>
+        <>
+            <div className="subproduct_wrap">
+                {
+                    servData.map((item, index) =>
+                        <Slot2 key={index} item={item} index={index} />
+                    )
+                }
+            </div>
+        </>
     );
 }
