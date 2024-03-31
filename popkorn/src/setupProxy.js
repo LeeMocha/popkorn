31ce50124ac0b8de2cfef93da44528d11bc13dbe
DESKTOP-VL5PSTN\c <qwertui1217@gmail.com>
2024-04-01 03:30:07 +0900

    2024-04-01 배포중 CORS 만남.. ec2 스프링, mysql 배포 완료

--- popkorn/src/setupProxy.js
+++ popkorn/src/setupProxy.js
@@ -1,10 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
+  
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8080',
+      target: 'http://15.164.171.73:8080',
       changeOrigin: true,
     })
   );
