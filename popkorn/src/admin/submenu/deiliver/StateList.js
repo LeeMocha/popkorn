7069da3ac3422c704da6770c3fdeb408475631e8
DESKTOP-G8524NV\이혜나 <dlgpsk1112@naver.com>
2024-04-01 09:22:45 +0900

    2024-04-01 만우절

--- popkorn/src/admin/submenu/deiliver/StateList.js
+++ popkorn/src/admin/submenu/deiliver/StateList.js
@@ -22,24 +22,47 @@ export default function StateList() {
                     ))
                     }
                 </div>
-                
+
                 <div className='stateDelivery'>
                     <h3><i className='xi-spinner-1 xi-spin'></i>Delivery progress</h3>
-                    <div className='stateProgress'>
-                        <div className='arrow'>
-                            <span><i className='xi-check'></i></span>
+                    <div className='stateProgressarrow'>
+                        <div className='stateProgress'>
+                            <div className='arrow'>
+                                <span><i className='xi-check'></i></span>
+                            </div>
+                            <p />
+                            <div className='arrow'>
+                                <span><i className='xi-check'></i></span>
+                            </div>
+                            <p />
+                            <div className='arrow'>
+                                <span><i className='xi-check'></i></span>
+                            </div>
+                            <p />
+                            <div className='arrow'>
+                                <span><i className='xi-check'></i></span>
+                            </div>
                         </div>
-                        <p />
-                        <div className='arrow'>
-                            <span><i className='xi-check'></i></span>
-                        </div>
-                        <p />
-                        <div className='arrow'>
-                            <span><i className='xi-check'></i></span>
-                        </div>
-                        <p />
-                        <div className='arrow'>
-                            <span><i className='xi-check'></i></span>
+                    </div>
+
+                    <div className='stepByStep'>
+                        <div className='stepByStepStage'>
+                            <div className='stage'>
+                                <h4>Order Process</h4>
+                                <p>주문날짜 : </p>
+                            </div>
+                            <div className='stage'>
+                                <h4>Packaging progress</h4>
+                                <p>포장날짜 : </p>
+                            </div>
+                            <div className='stage'>
+                                <h4>Shipment Start</h4>
+                                <p>배송시작날짜 : </p>
+                            </div>
+                            <div className='stage'>
+                                <h4>Delivery completed</h4>
+                                <p>배송완료날짜 : </p>
+                            </div>
                         </div>
                     </div>
                 </div>
