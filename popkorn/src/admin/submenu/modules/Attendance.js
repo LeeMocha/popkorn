00e1aef23925e2ef3fa8604b5a7744e3b01422c3
DESKTOP-VL5PSTN\c <qwertui1217@gmail.com>
2024-04-13 23:43:02 +0900

    2024-04-13

--- popkorn/src/admin/submenu/modules/Attendance.js
+++ popkorn/src/admin/submenu/modules/Attendance.js
@@ -4,6 +4,8 @@ import { QRCodeCanvas } from 'qrcode.react';
 export default function Attendance() {
   const url = '';
 
+  // QR 출석체크 확인 로직 구현해야함
+
   return (
     <div>
       <QRCodeCanvas value={`http://3.35.11.217:8080/api/attendance/insert?id=${sessionStorage.getItem("loginID")}`} />
