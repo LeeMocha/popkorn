d0f806589136990ca6edc4624ad8aa8bb12d50c2
User -2023NGLAA\Administrator <qwertui1217@gmail.com>
2024-04-15 17:52:27 +0900

    2024-04-15

--- popkorn/src/refund/Refund.js
+++ popkorn/src/refund/Refund.js
@@ -74,7 +74,7 @@ export default function Refund() {
 
             // 선택s된 상품들이 있는지 확인
             if (selectedItems.length > 0) {
-                apiCall("/api/pay/retund","POST", orderinfo[0], null)
+                apiCall("/api/orderinfo/refundrequest","POST", orderinfo[0], null)
                 .then(response => {
                     if(response.data != null){
                         alert("The refund has been processed successfully. Redirecting to the main page.");
